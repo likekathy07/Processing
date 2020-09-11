76f75bc611e895892c3f3b35824e835273e0d09b
likekathy07 <70361647+likekathy07@users.noreply.github.com>
2020-09-11 14:46:36 +0900

    10129 채서영 9/11 코딩자료

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,18 @@
+function setup() {
+createCanvas(500, 500);
+
+  fill(255, 100, 100);
+  ellipse(100, 100, 50, 50);
+  fill(255, 255, 0);
+  ellipse(400,400,20,20);
+  fill(0,255,0);
+  ellipse(450,450,60,60);
+  fill(0,0,255);
+  ellipse(200, 200, 100, 100);
+
+}
+
+
+function draw() {
+
+}
